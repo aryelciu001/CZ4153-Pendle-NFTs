5a3e449f1615f6129c893c3862e9538f04b44f5f
aryelciu001 <aryelciu001@gmail.com>
2021-10-27 10:06:25 +0800

    ADD: docs and tidy up test script

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -2,113 +2,109 @@ import { expect } from "chai";
 import { Signer, ContractFactory, Contract } from "ethers";
 import { ethers } from "hardhat";
 
-const contractName: string = "PendleItemFactory";
+const pendleItemFactoryContractName: string = "PendleItemFactory";
 const pendleContractName: string = "PENDLE";
 const pendleLiquidityContractName: string = "PendleLiquidityMiningBaseV2";
 const zeroAddress: string = "0x0000000000000000000000000000000000000000";
 
 describe("PendleItemFactory", async function () {
+  // accounts
   let owner: Signer; 
   let addr1: Signer; 
   let addr2: Signer;
-  let PendleItemFactory: ContractFactory;
-  let pif: Contract;
+
+  // accounts' address
   let ownerAddress: string;
   let addr1Address: string;
   let addr2Address: string;
-  let pendleTokenAddress: string;
-  let liquidityMiningAddress: string;
-  
-  it("Deploying smart contract...", async function () {
-    PendleItemFactory = await ethers.getContractFactory(contractName);
-    pif = await PendleItemFactory.deploy();
+
+  // Contract Factory
+  let PendleItemFactory: ContractFactory;
+  let Pendle: ContractFactory;
+  let PendleLiquidityMining: ContractFactory;
+
+  // Contract
+  let pendleItemFactoryContract: Contract;
+  let pendleContract: Contract;
+  let pendleLiquidityMiningContract: Contract;
+
+
+  it("Should deploy pendle item contract...", async function () {
+    PendleItemFactory = await ethers.getContractFactory(pendleItemFactoryContractName);
+    pendleItemFactoryContract = await PendleItemFactory.deploy();
     [owner, addr1, addr2] = await ethers.getSigners();
     ownerAddress = await owner.getAddress();
     addr1Address = await addr1.getAddress();
     addr2Address = await addr2.getAddress();
-    await pif.deployed();
+    await pendleItemFactoryContract.deployed();
   }).timeout(10000);
   
-  it("Create new items...", async function () {
-    await pif.createNewItem("first")
-    await pif.createNewItem("second")
-    await pif.createNewItem("third")
+  it("Should create new items...", async function () {
+    await pendleItemFactoryContract.createNewItem("first")
+    await pendleItemFactoryContract.createNewItem("second")
+    await pendleItemFactoryContract.createNewItem("third")
   })
   
-  it("Transfer items...", async function () {
+  it("should transfer items...", async function () {
     // transfer directly from owner to addr1
-    await pif["safeTransferFrom(address,address,uint256)"](ownerAddress, addr1Address, 1)
+    await pendleItemFactoryContract["safeTransferFrom(address,address,uint256)"](ownerAddress, addr1Address, 1)
     // approve addr1 to move token 2
-    await pif["approve(address,uint256)"](addr1Address, 2)
+    await pendleItemFactoryContract["approve(address,uint256)"](addr1Address, 2)
     // transfer from owner to addr2, called by addr1
-    await pif.connect(addr1)["safeTransferFrom(address,address,uint256)"](ownerAddress, addr2Address, 2)
+    await pendleItemFactoryContract.connect(addr1)["safeTransferFrom(address,address,uint256)"](ownerAddress, addr2Address, 2)
   })
 
-  it("Check Owner...", async function () {
-    expect(await pif.ownerOf(0)).to.equal(ownerAddress)
-    expect(await pif.ownerOf(1)).to.equal(addr1Address)
-    expect(await pif.ownerOf(2)).to.equal(await addr2.getAddress())
+  it("should check owner...", async function () {
+    expect(await pendleItemFactoryContract.ownerOf(0)).to.equal(ownerAddress)
+    expect(await pendleItemFactoryContract.ownerOf(1)).to.equal(addr1Address)
+    expect(await pendleItemFactoryContract.ownerOf(2)).to.equal(await addr2.getAddress())
   })
 
-  it("Deploying PENDLE contract...", async function () {
-    let Pendle = await ethers.getContractFactory(pendleContractName);
+  it("should deploy PENDLE contract...", async function () {
+    Pendle = await ethers.getContractFactory(pendleContractName);
     [owner, addr1, addr2] = await ethers.getSigners();
-    pif = await Pendle.deploy(
+    pendleContract = await Pendle.deploy(
       ownerAddress, 
       ownerAddress, 
       ownerAddress, 
       ownerAddress, 
       ownerAddress
     );
-    pendleTokenAddress = pif.address
-    await pif.deployed()
+    await pendleContract.deployed()
 
     // by now, owner must have 188,700,000 pendle tokens
-    let ownerCoin = await pif["balanceOf(address)"](ownerAddress)
+    let ownerCoin = await pendleContract["balanceOf(address)"](ownerAddress)
     ownerCoin = ethers.utils.formatEther(ownerCoin)
     ownerCoin = Number(ownerCoin)
     expect(ownerCoin).to.equal(188700000)
 
     // transfer 10 pendle tokens to addr1 
-    await pif["transfer(address,uint256)"](addr1Address, ethers.utils.parseEther("10"))
+    await pendleContract["transfer(address,uint256)"](addr1Address, ethers.utils.parseEther("10"))
     
     // now addr1 have 10 tokens
-    let addr1Coin = await pif["balanceOf(address)"](addr1Address)
+    let addr1Coin = await pendleContract["balanceOf(address)"](addr1Address)
     addr1Coin = ethers.utils.formatEther(addr1Coin)
     addr1Coin = Number(addr1Coin)
     expect(addr1Coin).to.equal(10)
   }).timeout(10000);
 
-  it("Deploying PENDLE LIQUIDITY MINING contract...", async function () {
-    let PendleLiquidityMining = await ethers.getContractFactory(pendleLiquidityContractName);
-    pif = await PendleLiquidityMining.deploy(
-      ownerAddress, 
-      ownerAddress, 
-      zeroAddress, 
-      pendleTokenAddress, 
-      pendleTokenAddress,
-      zeroAddress,
-      Math.floor(Date.now() / 1000) + 60,
-      10,
-      10
+  it("should deploy PENDLE LIQUIDITY MINING contract...", async function () {
+    PendleLiquidityMining = await ethers.getContractFactory(pendleLiquidityContractName);
+    pendleLiquidityMiningContract = await PendleLiquidityMining.deploy(
+      ownerAddress, // _governanceManager
+      ownerAddress, // _pausingManager
+      zeroAddress, // _whitelist
+      pendleContract.address, // _pendleTokenAddress
+      pendleContract.address, // _stakeToken
+      zeroAddress, // _yieldToken
+      Math.floor(Date.now() / 1000) + 60, // _startTime (in unix, now + 60 seconds)
+      60*60*24, // _epochDuration (24 hours)
+      5 // _vestingEpochs
     );
-    liquidityMiningAddress = pif.address
-    await pif.deployed();
-    console.log(ownerAddress, 
-      ownerAddress, 
-      zeroAddress, 
-      pendleTokenAddress, 
-      pendleTokenAddress,
-      zeroAddress,
-      Math.floor(Date.now() / 1000) + 60,
-      10,
-      10)
+    await pendleLiquidityMiningContract.deployed();
   }).timeout(10000);
 
-  // it("Stake pendle token...", async function () {
-  //   let stakerAddress = addr2Address
-  //   expect(liquidityMiningAddress).to.equal(pif.address)
-  //   console.log(stakerAddress, stakerAddress, ethers.utils.parseEther("10"))
-  //   // await pif["stake(address,uint256)"](stakerAddress, ethers.utils.parseEther("10"))
-  // })
+  it("should stake pendle token...", async function () {
+    // await pendleLiquidityMiningContract.connect(addr2)["stake(address,uint256)"](addr2Address, ethers.utils.parseEther("10"))
+  })
 });
